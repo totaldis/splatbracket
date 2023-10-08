63fca5932b7fb8a48c4275ed3fc9ad89da4925d2
Aaron VanEtveldt <totaldis@gmail.com>
2023-10-07 20:21:08 -0400

    still good?

--- app/page.js
+++ app/page.js
@@ -9,7 +9,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
+          Get farted by editing&nbsp;
           <code className={styles.code}>app/page.js</code>
         </p>
         <div>
